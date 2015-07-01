2252a2e484ac17a28e206f69978a4bb2f62e8ccf
Willie Long <Donatello@Wills-Macbook.local>
2015-06-30 23:52:51 -0400

    automated push

--- apertureDomainProcessing.m
+++ apertureDomainProcessing.m
@@ -1,20 +1,38 @@
 clear all;
-% close all;
+close all;
 clc
-rootpth = '/getlab/wjl11/scratch';
-addpath([rootpth '/data_files/beamforming/ece582_final_project/'])
-addpath([rootpth '/data_files/beamforming/DR_testdata/'])
+% rootpth = '/getlab/wjl11/scratch';
+rootpth = '..';
+addpath([rootpth '/data_files/beamforming/'])
+addpath([rootpth '/data_files/beamforming/ece582_final_project'])
+% addpath([rootpth '/data_files/beamforming/DR_testdata/'])
 addpath ../beamforming/accessory/
 addpath ../beamforming/core/    
 load point_target
 % load DR_testdata
-load les
+% load les
 
+% load sampleData.mat
+% rf = double(data.channel_data);
+% x = axes.x;
+% z = axes.z;
 [~, rf_steer, x, z] = linearScanDR(rf,acq_params,bf_params);
 
 f0=7e6;
 fs=100e6;
-lags = 80;
+% lags = size(rf_steer,3);
+lags = size(rf_steer,2);
+
+% fs = data.fs;
+% f0 = data.f0;
+
+% SNR = 6;
+% noiseraw = 10^(-SNR/20)*max(rf(:)).*randn(size(rf));
+% bw = 0.85*f0;
+% 
+% [bf,af] = butter(2,[f0-bw/2 f0+bw/2]./(fs/2),'bandpass');
+% noisefilt = filtfilt(bf,af,noiseraw);
+% rf_steer = rf + noisefilt;
 
 % excitation=sin(2*pi*f0*(0:1/fs:1/(f0*4)));
 excitation = [0.5,0,-0.5];
@@ -56,19 +74,19 @@ subplot(122)
 imagesc(fft_filt./max(fft_filt(:))); 
 
 figure
-subplot(121)
-rf2bmode(cont_sum, 45, x, z); colormap jet
+% subplot(121)
+% rf2bmode(cont_sum, 40, x, z); colormap gray
 % imagesc(cont_sum); colormap jet
-% subplot(211)
-% rf2bmode(cont_sum, 80, x, z); colormap jet
+subplot(211)
+rf2bmode(cont_sum, 80, x, z); colormap jet
 % xlim([-2 2])
 % ylim([min(z)*1000 46])
-
-subplot(122)
-rf2bmode(filt_sum, 45, x, z); colormap jet
+% 
+% subplot(122)
+% rf2bmode(filt_sum, 40, x, z); colormap gray
 % imagesc(filt_sum); colormap jet
-% subplot(212)
-% rf2bmode(filt_sum, 80, x, z); colormap jet
+subplot(212)
+rf2bmode(filt_sum, 80, x, z); colormap jet
 % xlim([-2 2])
 % ylim([min(z)*1000 46])
 
